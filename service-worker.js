a2e9b33814bf490e56c3ed3ba624a48cbf06aacd
alexanderialpan-debug <alexanderialpan@gmail.com>
2026-05-03 14:19:01 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,36 @@
+const CACHE_NAME = "alpine-capital-v1";
 
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/portal.html",
+  "/logo.png",
+  "/favicon.svg",
+  "/favicon-32x32.png",
+  "/android-chrome-192x192.png",
+  "/android-chrome-512x512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
